6a3496caba23173d6a435c7abc4646e97738ffd2
Ashutosh patel <patelaashu116@gmail.com>
2026-08-12 00:59:59 +0530

    Prediction Pipeline

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -92,8 +92,7 @@ class DataTransformation:
                 f"Applying preprocessing object on training dataframe and testing dataframe . "
             )
             
-            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)
-            
+            input_feature_train_arr = preprocessing_obj.fit_transform(input_feature_train_df)   
             input_feature_test_arr = preprocessing_obj.transform(input_feature_test_df)
             
             train_arr = np.c_[
